219f74ae1dbc7c0fd9edf3b34e21a9149f895028
Andrey Kumanyaev <me@zzet.org>
2012-10-16 00:48:48 +0400

    add new scope methods

--- lib/usefull_scopes.rb
+++ lib/usefull_scopes.rb
@@ -50,6 +50,25 @@ module UsefullScopes
         quoted_term = connection.quote(term + '%')
         where("#{quoted_table_name}.#{a} ilike #{quoted_term}")
       }
+      scope "#{a}_more", ->(value_or_object) {
+        value = find_object_value_or_value(value_or_object, a)
+        where("#{quoted_table_name}.#{a} > #{value}")
+      }
+      scope "#{a}_less", ->(value_or_object) {
+        value = find_object_value_or_value(value_or_object, a)
+        where("#{quoted_table_name}.#{a} < #{value}")
+      }
+      scope "#{a}_more_or_equal", ->(value_or_object) {
+        value = find_object_value_or_value(value_or_object, a)
+        where("#{quoted_table_name}.#{a} >= #{value}")
+      }
+      scope "#{a}_less_or_equal", ->(value_or_object) {
+        value = find_object_value_or_value(value_or_object, a)
+        where("#{quoted_table_name}.#{a} <= #{value}")
+      }
+    end
+    def find_object_value_or_value(value_or_object, field = "id")
+      value_or_object.is_a?(ActiveRecord::Base) ? value_or_object.send(field) : value_or_object
     end
   end
 end
