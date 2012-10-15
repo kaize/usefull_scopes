01ee62115c23e39d78195c3156eccfb317ed42fc
Andrey Kumanyaev <me@zzet.org>
2012-10-16 00:51:18 +0400

    add blank lines between scopes (more readable)

--- lib/usefull_scopes.rb
+++ lib/usefull_scopes.rb
@@ -40,32 +40,40 @@ module UsefullScopes
 
     attribute_names.each do |a|
       scope "by_#{a}", order("#{quoted_table_name}.#{a} DESC")
+
       scope "asc_by_#{a}", order("#{quoted_table_name}.#{a} ASC")
+
       scope "like_by_#{a}", ->(term) {
         quoted_term = connection.quote(term + '%')
         where("lower(#{quoted_table_name}.#{a}) like #{quoted_term}")
       }
+
       scope "ilike_by_#{a}", ->(term) {
         quoted_term = connection.quote(term + '%')
         where("#{quoted_table_name}.#{a} ilike #{quoted_term}")
       }
+
       scope "#{a}_more", ->(value_or_object) {
         value = find_object_value_or_value(value_or_object, a)
         where("#{quoted_table_name}.#{a} > #{value}")
       }
+
       scope "#{a}_less", ->(value_or_object) {
         value = find_object_value_or_value(value_or_object, a)
         where("#{quoted_table_name}.#{a} < #{value}")
       }
+
       scope "#{a}_more_or_equal", ->(value_or_object) {
         value = find_object_value_or_value(value_or_object, a)
         where("#{quoted_table_name}.#{a} >= #{value}")
       }
+
       scope "#{a}_less_or_equal", ->(value_or_object) {
         value = find_object_value_or_value(value_or_object, a)
         where("#{quoted_table_name}.#{a} <= #{value}")
       }
     end
+
     def find_object_value_or_value(value_or_object, field = "id")
       value_or_object.is_a?(ActiveRecord::Base) ? value_or_object.send(field) : value_or_object
     end
