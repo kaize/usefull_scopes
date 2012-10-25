ad29383b92dca057c2ed71a48355c362e7bf400b
Anton Kalyaev <anton.kalyaev@gmail.com>
2012-10-25 23:58:26 +0400

    added with and without attribute scopes

--- lib/usefull_scopes.rb
+++ lib/usefull_scopes.rb
@@ -16,6 +16,7 @@ module UsefullScopes
     attribute_names.each do |a|
       scope "by_#{a}", order("#{quoted_table_name}.#{a} DESC")
       scope "asc_by_#{a}", order("#{quoted_table_name}.#{a} ASC")
+
       scope "like_by_#{a}", lambda {|term|
         quoted_term = connection.quote(term + '%')
         where("lower(#{quoted_table_name}.#{a}) like #{quoted_term}")
@@ -24,6 +25,9 @@ module UsefullScopes
         quoted_term = connection.quote(term + '%')
         where("#{quoted_table_name}.#{a} ilike #{quoted_term}")
       }
+
+      scope "with_#{a}", lambda { |value| where("#{quoted_table_name}.#{a} = ?", value) }
+      scope "without_#{a}", where("#{quoted_table_name}.#{a} IS NULL")
     end
   end
 end
