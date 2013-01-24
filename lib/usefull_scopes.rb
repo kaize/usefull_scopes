8308162bcd2229e5197e15c3003dc5d5ac23d078
Andrey Kumanyaev <me@zzet.org>
2013-01-24 23:50:11 +0400

    save

--- lib/usefull_scopes.rb
+++ lib/usefull_scopes.rb
@@ -40,6 +40,77 @@ module UsefullScopes
       where arel_table.create_and query_params
     }
 
+    scope :desc_by, ->(attrs) {
+      order(collect_order_query_rules(attrs, :desc))
+    }
+
+    scope :asc_by, ->(attrs) {
+      order(collect_order_query_rules(attrs, :asc))
+    }
+
+    scope :more_than, ->(attrs) {
+      where_and(attrs, :gt)
+    }
+
+    scope :less_than, ->(attrs) {
+      where_and(attrs, :lt)
+    }
+
+    scope :more_or_equal, ->(attrs) {
+      where_and(attrs, :gteq)
+    }
+
+    scope :less_or_equal, ->(attrs) {
+      where_and(attrs, :lteq)
+    }
+
+    def self.collect_order_query_rules(array, rule)
+      query_params = []
+
+      attrs =
+        case array
+        when Array
+          array
+        when Symbol
+          [array]
+        else
+          raise TypeError, "Symbol or Array of symbols is expected"
+        end
+
+      attrs.each do |attr_name, attr_value|
+        query_params << arel_table[attr_name].send(rule)
+      end
+
+      raise ArgumentError, "Empty Array" if query_params.blank?
+
+      query_params
+    end
+
+    def self.collect_query_rules(hash_or_object, rule, field = :id)
+      query_params = []
+
+      attrs =
+        case hash_or_object
+        when ActiveRecord::Base
+          { field => hash_or_object.send(field) }
+        when Hash
+          hash_or_object
+        when Array
+          
+        else
+          raise TypeError, "Hash or AR object is expected"
+        end
+
+      attrs.each do |attr_name, attr_value|
+        attr_value = attr_value.send(attr_name) if attr_value.is_a? ActiveRecord::Base
+        query_params << arel_table[attr_name].send(rule, attr_value)
+      end
+
+      raise ArgumentError, "Empty Hash" if query_params.blank?
+
+      query_params
+    end
+
     attribute_names.each do |a|
       a = a.to_sym
 
@@ -81,6 +152,12 @@ module UsefullScopes
     def self.find_object_value_or_value(value_or_object, field = :id)
       value_or_object.is_a?(ActiveRecord::Base) ? value_or_object.send(field) : value_or_object
     end
+
+    def self.where_and(attrs, rule)
+      query_params = collect_query_rules(attrs, rule)
+      where arel_table.create_and query_params
+    end
+
   end
 end
 
